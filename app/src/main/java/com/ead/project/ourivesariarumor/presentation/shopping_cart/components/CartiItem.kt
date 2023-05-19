26da05ce1cf260251a91f906dbbd119e7d0081d1
alex_ <alex_torres-xc@hotmail.com>
2023-05-18 22:55:21 -0500

    cart item todo

---
+++ app/src/main/java/com/ead/project/ourivesariarumor/presentation/shopping_cart/components/CartiItem.kt
@@ -0,0 +1,13 @@
+package com.ead.project.ourivesariarumor.presentation.shopping_cart.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.ead.project.ourivesariarumor.domain.model.Cart
+
+@Composable
+fun CartItem(
+    modifier: Modifier = Modifier,
+    cart: Cart
+) {
+    
+}
