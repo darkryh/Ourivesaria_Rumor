d29bf31812229281221e91ef810520910e8c7504
alex_ <alex_torres-xc@hotmail.com>
2023-05-19 11:26:26 -0500

    parent data class for inventory

---
+++ app/src/main/java/com/ead/project/ourivesariarumor/domain/use_case/InventoryUseCase.kt
@@ -0,0 +1,7 @@
+package com.ead.project.ourivesariarumor.domain.use_case
+
+import com.ead.project.ourivesariarumor.domain.use_case.inventory.AddProduct
+
+data class InventoryUseCase(
+    val addProduct: AddProduct
+)
