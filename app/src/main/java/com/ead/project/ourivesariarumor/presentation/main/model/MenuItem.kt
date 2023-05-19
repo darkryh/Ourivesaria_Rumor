2b2a12bd95502582d3b40abf770f7fb6c286bae1
alex_ <alex_torres-xc@hotmail.com>
2023-05-19 11:31:00 -0500

    message

---
+++ app/src/main/java/com/ead/project/ourivesariarumor/presentation/main/model/MenuItem.kt
@@ -0,0 +1,10 @@
+package com.ead.project.ourivesariarumor.presentation.main.model
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class MenuItem(
+    val id : String,
+    val title : String,
+    val contentDescription : String,
+    val icon : ImageVector
+)
